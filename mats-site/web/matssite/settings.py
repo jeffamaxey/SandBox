7f984effb11cc7447901655d65c9a3d3ef756b83
holmen1 <holmen1@gmail.com>
2020-05-11 10:56:20 +0200

    Add prod Dockerfile

--- mats-site/web/matssite/settings.py
+++ mats-site/web/matssite/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-#ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-
 
 # Application definition
 
